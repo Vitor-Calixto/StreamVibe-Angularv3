20638215fc85248ef6360f58e0b0d82fb79dd066
death-angel2 <vitorpedrocalixto@gmail.com>
2025-07-10 21:40:23 -0300

    StreamVibe-Angularv3

--- src/app/componentes/filme/filme.ts
+++ src/app/componentes/filme/filme.ts
@@ -7,20 +7,27 @@ import { Component } from '@angular/core';
   styleUrl: './filme.css'
 })
 export class Filme {
+
+alternarFavorito(){
+  this.filme1.favorito = !this.filme1.favorito
+}
+
   filme1 = {
 nome: "Tropa de Elite",
 imagem: "/tp.jpg",
-sinopse:"Vão passar uns traficantes para o papa dormir no Morro"
+sinopse:"Vão passar uns traficantes para o papa dormir no Morro",
+favorito: false
+
   }
-  filme2 = {
-nome: "Truque de Mestre",
-imagem:  "/truque de mstre.jpg",
-sinopse:"Mágicos que assaltam o banco e fazem o hulk de trouxa com ajuda do Morgan Freeman"
-  }
-  filme3 = {
-nome: "300",
-imagem:  "/300.jpg",
-sinopse:"Só nego brabo indo p guerra proteger sua cidade contra um Africano safado "
-  }
+//   filme2 = {
+// nome: "Truque de Mestre",
+// imagem:  "/truque de mstre.jpg",
+// sinopse:"Mágicos que assaltam o banco e fazem o hulk de trouxa com ajuda do Morgan Freeman"
+//   }
+//   filme3 = {
+// nome: "300",
+// imagem:  "/300.jpg",
+// sinopse:"Só nego brabo indo p guerra proteger sua cidade contra um Africano safado "
+//   }
 
 }
