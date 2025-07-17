fa2707d87d9f00efbf6a4fecf26771d83a4c6d70
death-angel2 <vitorpedrocalixto@gmail.com>
2025-07-17 20:29:56 -0300

    Alterações do dia 17/07

--- src/app/componentes/filme/filme.ts
+++ src/app/componentes/filme/filme.ts
@@ -19,15 +19,33 @@ sinopse:"Vão passar uns traficantes para o papa dormir no Morro",
 favorito: false
 
   }
-//   filme2 = {
-// nome: "Truque de Mestre",
-// imagem:  "/truque de mstre.jpg",
-// sinopse:"Mágicos que assaltam o banco e fazem o hulk de trouxa com ajuda do Morgan Freeman"
-//   }
-//   filme3 = {
-// nome: "300",
-// imagem:  "/300.jpg",
-// sinopse:"Só nego brabo indo p guerra proteger sua cidade contra um Africano safado "
-//   }
+  filme2 = {
+nome: "Truque de Mestre",
+imagem:  "/truque de mstre.jpg",
+sinopse:"Mágicos que assaltam o banco e fazem o hulk de trouxa com ajuda do Morgan Freeman"
+  }
+  filme3 = {
+nome: "300",
+imagem:  "/300.jpg",
+sinopse:"Só nego brabo indo p guerra proteger sua cidade contra um Africano safado "
+  }
+
+  filme4 = {
+nome: "Coringa",
+imagem:  "/coringa.jpg",
+sinopse:"Um comediante que teve um dia ruim e se vinga matando todo mundo "
+  }
+
+  filme5 = {
+nome: "Toy Story",
+imagem:  "/toyStory.jpg",
+sinopse:"Um bando de boneco macabro competindo p ficar no quarto de um moleque"
+  }
+
+  filme6 = {
+nome: "Velozes e Furiosos",
+imagem:  "/velozes e furiosos.jpg",
+sinopse:"ta tudo tranquilo,quando surge algo que tira o the Rock da paz dele e obriga ele a voltar para a vida de assasino  "
+  }
 
 }
